8252ba86ab753bdd3626b65fc655e74e6ce17bb0
eduvs68 <eduvs68@gmail.com>
2024-10-03 23:08:20 -0300

    Quarto commit, agora a API puxa as infos dos personagens e todas essas infos estao em portugues

--- projeto/script.js
+++ projeto/script.js
@@ -25,10 +25,7 @@ async function loadCharacters(url) {
 
     responseJson.results.forEach((character) => {
       const card = document.createElement("div"); //Cria uma div
-      card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(
-        /\D/g,
-        ""
-      )}.jpg')`;
+      card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g,"")}.jpg')`;
       card.className = "cards";
 
       const characterNameBG = document.createElement("div");
@@ -44,7 +41,43 @@ async function loadCharacters(url) {
       card.onclick = () => {
         const modal = document.getElementById("modal");
         modal.style.visibility = "visible";
-      }
+
+        const modalContent = document.getElementById("modal-content");
+        modalContent.innerHTML = '';
+
+        const characterImage = document.createElement("div");
+        characterImage.style.backgroundImage = 
+        `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g,"")}.jpg')`
+        characterImage.className = "character-image";
+
+        
+        const name = document.createElement("span");
+        name.className = "character-details";
+        name.innerText = `Nome: ${character.name}`;
+        
+        const height = document.createElement("span");
+        height.className = "character-details";
+        height.innerText = `Altura: ${convertHeight (character.height)}`;
+
+        const mass = document.createElement("span");
+        mass.className = "character-details";
+        mass.innerText = `Peso: ${convertMass(character.mass)}`;
+
+        const eyeColor = document.createElement("span");
+        eyeColor.className = "character-details";
+        eyeColor.innerText = `Cor dos olhos: ${convertEyeColor(character.eye_color)}`;
+
+        const birthYear = document.createElement("span");
+        birthYear.className = "character-details";
+        birthYear.innerText = `Nascimento: ${convertBirthYear(character.birth_year)}`;
+
+        modalContent.appendChild(characterImage);
+        modalContent.appendChild(name);
+        modalContent.appendChild(height);
+        modalContent.appendChild(mass);
+        modalContent.appendChild(eyeColor);
+        modalContent.appendChild(birthYear);
+    }
 
       mainContent.appendChild(card);
     });
@@ -93,4 +126,42 @@ async function loadPreviousPage() {
 function hideModal() {
   const modal = document.getElementById("modal");
   modal.style.visibility = "hidden";
+}
+
+function convertEyeColor(eyeColor) {
+    const cores = {
+        blue: "azul",
+        brown: "marrom",
+        green: "verde",
+        yellow: "amarelo",
+        black: "preto",
+        pink: "rosa",
+        red: "vermelho",
+        orange: "laranja",
+        hazel: "avela",
+        unknown: "desconhecida"
+    }
+
+    return cores[eyeColor.toLowerCase()] || eyeColor;
+}
+
+function convertHeight(height) {
+    if (height === "unknown"){
+        return "desconhecida"
+    }
+    return (height / 100).toFixed(2)
+}
+
+function convertMass (mass){
+    if (mass === "unknown"){
+        return "desconhecido"
+    }
+    return `${mass} kg`
+}
+
+function convertBirthYear(birthYear){
+    if (birthYear === "unknown"){
+        return "desconhecido";
+    }
+    return birthYear;
 }
